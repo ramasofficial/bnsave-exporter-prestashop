bf1cd1c7bc8581995793e7f1a64f4e7bb6a74197
Ramas Grikštas <ramas.grikstas@kilo.health>
2024-03-05 00:15:15 +0200

    Try to implement the module

--- bnsaveexporter.php
+++ bnsaveexporter.php
@@ -9,13 +9,13 @@ class BnsaveExporter extends Module
     public function __construct()
     {
         $this->name = 'bnsaveexporter';
-        $this->tab = 'administration';
+        $this->tab = 'migration_tools';
         $this->version = '1.0.0';
-        $this->author = 'Bnsave';
+        $this->author = 'Bnsave (www.bnsave.lt)';
         $this->need_instance = 1;
         $this->ps_versions_compliancy = [
             'min' => '1.6.0.0',
-            'max' => '8.99.99',
+            'max' => _PS_VERSION_,
         ];
         $this->bootstrap = true;
 
@@ -29,5 +29,33 @@ class BnsaveExporter extends Module
         if (!Configuration::get('BNSAVEEXPORTER_NAME')) {
             $this->warning = $this->trans('No name provided', [], 'Modules.Mymodule.Admin');
         }
+    }
+
+    public function install()
+    {
+        return parent::install() && $this->createDir();
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall() && $this->removeDir();
+    }
+
+    private function createDir()
+    {
+        $export_directory = _PS_IMG_DIR_ . 'upload/bnsave-exports';
+
+        if (!mkdir($export_directory, 0755, true) && !is_dir($export_directory)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function removeDir()
+    {
+        $export_directory = _PS_IMG_DIR_ . 'upload/bnsave-exports';
+
+        return unlink($export_directory);
     }
 }
