d8edf9d47b229e9d4a5fa81ca29d864f932a46df
Ramas Grikštas <ramas.grikstas@kilo.health>
2024-03-05 10:10:11 +0200

    Controller implementation

--- bnsaveexporter.php
+++ bnsaveexporter.php
@@ -4,8 +4,11 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class BnsaveExporter extends Module
+final class bnsaveexporter extends Module
 {
+    const EXPORT_DIRECTORY = _PS_IMG_DIR_ . 'upload/bnsave-exports';
+    const EXPORT_FILE = 'discounts.json';
+
     public function __construct()
     {
         $this->name = 'bnsaveexporter';
@@ -33,29 +36,51 @@ class BnsaveExporter extends Module
 
     public function install()
     {
-        return parent::install() && $this->createDir();
+        return parent::install() && $this->createAssets() && $this->configure();
     }
 
     public function uninstall()
     {
-        return parent::uninstall() && $this->removeDir();
+        return parent::uninstall() && $this->removeAssets();
     }
 
-    private function createDir()
+    private function createAssets()
     {
-        $export_directory = _PS_IMG_DIR_ . 'upload/bnsave-exports';
+        if (!file_exists(self::EXPORT_DIRECTORY) && !mkdir(self::EXPORT_DIRECTORY, 0755, true) && !is_dir(self::EXPORT_DIRECTORY)) {
+            return false;
+        }
 
-        if (!file_exists($export_directory) && !mkdir($export_directory, 0755, true) && !is_dir($export_directory)) {
+        try {
+            file_put_contents($this->getExportFilePath(), json_encode([]));
+        } catch (Exception $exception) {
             return false;
         }
 
         return true;
     }
 
-    private function removeDir()
+    private function removeAssets()
     {
-        $export_directory = _PS_IMG_DIR_ . 'upload/bnsave-exports';
+        try {
+            unlink($this->getExportFilePath());
+        } catch (Exception $exception) {
+            // Do nothing, file doesn't exist already
+        }
 
-        return unlink($export_directory);
+        return true;
+    }
+
+    private function getExportFilePath()
+    {
+        return self::EXPORT_DIRECTORY . '/' . self::EXPORT_FILE;
+    }
+
+    private function configure()
+    {
+        Configuration::updateValue('BNSAVEEXPORTER_SHOP_NAME', 'My Shop');
+        Configuration::updateValue('BNSAVEEXPORTER_CATEGORY_MAPPING', []);
+        Configuration::updateValue('BNSAVEEXPORTER_EXCLUDE_TAGS', '');
+
+        return true;
     }
 }
---
+++ controllers/front/validation.php
@@ -0,0 +1,41 @@
+<?php
+
+class BnsaveExporterValidationModuleFrontController  extends ModuleFrontController
+{
+    public $auth = false;
+    public $ajax;
+
+    public function initContent()
+    {
+        $this->ajax = true;
+
+        $action = Tools::getValue('action');
+
+        if ($action === 'json') {
+            $this->printJson();
+            exit;
+        }
+
+        if ($action === 'cron') {
+            $this->cron();
+            exit;
+        }
+
+        $this->ajaxRender('No route found.');
+    }
+
+    private function printJson()
+    {
+        $contents = file_get_contents(bnsaveexporter::EXPORT_DIRECTORY . '/' . bnsaveexporter::EXPORT_FILE);
+
+        header('Content-Type: application/json');
+
+        echo $contents;
+    }
+
+    private function cron()
+    {
+        // TODO
+        echo 'cron';
+    }
+}
